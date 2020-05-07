9ea56d3cd57c4e3b012d15c3b9ad088e08b57391
Subham Das <subhamdas226@gmail.com>
2020-05-07 15:20:40 +0530

    FSJ0320B002

--- src/utility/OffensiveWordsInterface.java
+++ src/utility/OffensiveWordsInterface.java
@@ -1,1 +1,15 @@
+/*
+ * Progression - 2
+Create an Interface - public interface OffensiveWordsInterface.
+Create two methods boolean checkBlogTitle(Blog blog) and boolean checkBlogDescription(Blog blog).
+ */
 package utility;
+
+import model.Blog;
+
+public interface OffensiveWordsInterface{
+	
+	boolean checkBlogTitle(Blog blog);
+	
+	boolean checkBlogDescription(Blog blog);
+}
