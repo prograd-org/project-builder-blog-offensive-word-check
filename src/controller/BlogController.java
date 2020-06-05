3f304285839caed9f587bf5d88f6343ef5e1d17e
swati2904 <swati.lpu17@gmail.com>
2020-06-06 03:09:19 +0530

    FSJ0520A108

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Blog;
 import model.User;
-
+import utility.CheckBlogPost;
 
 @WebServlet(urlPatterns= {"/blog"})
 public class BlogController extends HttpServlet {
@@ -48,7 +48,8 @@ public class BlogController extends HttpServlet {
 		blog.setBlogDescription(description);
 		blog.setDate(postedOn);
 
-		if(checkblog!) {
+		boolean checkblog = false;
+		if(checkblog == true) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
